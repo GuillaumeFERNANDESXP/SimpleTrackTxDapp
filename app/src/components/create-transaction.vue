f7c3aef9540c39efc27a17b390e356f95a729ca0
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2019-12-01 21:03:40 +0100

    Clean code Indentation

--- app/src/components/create-transaction.vue
+++ app/src/components/create-transaction.vue
@@ -2,37 +2,29 @@
   <div class="q-pa-md row items-start q-gutter-md flex flex-center">
     <q-card class="my-card">
       <q-card-section>
-            <p v-if="isInjected" id="has-metamask"><i aria-hidden="true" class="fa fa-check"></i> Metamask installed</p>
+        <p v-if="isInjected" id="has-metamask">
+          <i aria-hidden="true" class="fa fa-check"></i> Metamask installed
+        </p>
         <div class="text-h6">Create a TX</div>
       </q-card-section>
-   <q-form
-      @submit="onSubmit"
-      class="q-gutter-md"
-    >
-      <q-card-section>
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <q-card-section>
           <div class="row">
-              <div class="col ">
-            Amount to send :
-              </div>
-              <div class="col">
-        <q-input
-          outlined
-          />
-              </div>
+            <div class="col">Amount to send :</div>
+            <div class="col">
+              <q-input outlined />
+            </div>
           </div>
-          <p>To: </p>
-           <q-input
-          outlined
-          >
-          </q-input>
+          <p>To:</p>
+          <q-input outlined></q-input>
           <div>
-              <p></p>
+            <p></p>
           </div>
-          <div >
-        <q-btn label="Send" type="submit" color="primary"/>
-      </div>
-      </q-card-section>
-   </q-form>
+          <div>
+            <q-btn label="Send" type="submit" color="primary" />
+          </div>
+        </q-card-section>
+      </q-form>
     </q-card>
   </div>
 </template>
@@ -44,6 +36,6 @@ export default {
 
 <style>
 .q-card {
-    width: 30%;
+  width: 30%
 }
 </style>>
