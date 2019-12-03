3484162c36405effeef0129469b27ce83981127c
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2019-12-03 22:56:01 +0100

    ADD TRACKER TXgit add .

--- app/src/util/makeTx.js
+++ app/src/util/makeTx.js
@@ -1,33 +1,32 @@
 import Web3 from 'web3'
 
-let getTxHash = function () {
-  var web3 = new Web3(window.web3.currentProvider)
-  web3.eth.sendTransaction({
-    from: '0xB74fc3B69f626226f7F1c53D9D6D340AC291d481',
-    to: '0x1889EF49cDBaad420EB4D6f04066CA4093088Bbd',
-    value: '100178979780000'
-  })
-    .on('transactionHash', function (hash) {
-      console.log('THIS IS TXH HASH  ' + hash)
-      return hash
-    })
-}
+// let getTxHash = function () {
+//   var web3 = new Web3(window.web3.currentProvider)
+//   web3.eth.sendTransaction({
+//     from: '0xB74fc3B69f626226f7F1c53D9D6D340AC291d481',
+//     to: '0x1889EF49cDBaad420EB4D6f04066CA4093088Bbd',
+//     value: '100178979780000'
+//   })
+//     .on('transactionHash', function (hash) {
+//       console.log('THIS IS TXH HASH  ' + hash)
+//       return hash
+//     })
+// }
 
 let txHash = function () {
-  const web3 = new Web3(new Web3.providers.HttpProvider('ropsten.infura.io/v3/ecd08f482701425fbc7dd36db8c63358'))
-  const tx = getTxHash()
-  var subscription = web3.eth.subscribe('pendingTransactions', function (error, result) {
-    if (!error) {
-      console.log(result)
-    }
+  const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws'))
+  web3.eth.subscribe('pendingTransactions', (error, result) => {
+    if (!error) console.log('result: ', result)
+    console.log('error: ', error)
   })
-    .on('data', function (transaction) {
-      console.log(transaction)
+    .on('data', (log) => {
+      console.log('data: ', log)
     })
-  subscription.unsubscribe(function (error, success) {
-    if (success) {
-      console.log('Successfully unsubscribed!' + error)
-    }
-  })
+    .on('error', (log) => {
+      console.log('error: ', log)
+    })
 }
 export default txHash
+
+// You can create a stream of pending transactions using web3.eth.subscribe('pendingTransactions' [, callback]);, which currently returns a transaction hash.
+// You can turn into the actual transaction object using web3.eth.getTransaction(transactionHash [, callback]), which will return a transaction object.
